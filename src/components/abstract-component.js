374cb723c84409ab76eff0f8ba577745b05f1799
Роман <kromanjs@gmail.com>
2019-12-24 22:29:35 +0200

    7.2 Change screens

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -1,6 +1,9 @@
 import {createElement} from '../utils/render';
 
 
+const HIDDEN_CLASS = `visually-hidden`;
+
+
 export default class AbstractComponent {
   constructor() {
     if (new.target === AbstractComponent) {
@@ -23,5 +26,17 @@ export default class AbstractComponent {
 
   removeElement() {
     this._element = null;
+  }
+
+  show() {
+    if (this._element) {
+      this._element.classList.remove(HIDDEN_CLASS);
+    }
+  }
+
+  hide() {
+    if (this._element) {
+      this._element.classList.add(HIDDEN_CLASS);
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import SiteMenuComponent from './components/site-menu';
-import FilterController from './controllers/filter';
 import BoardComponent from './components/board';
 import BoardController from './controllers/board';
+import SiteMenuComponent, {MenuItem} from './components/site-menu';
+import FilterController from './controllers/filter';
+import StatisticsComponent from './components/statistics';
 import TasksModel from './models/tasks';
 import {generateTasks} from './mock/task';
 import {render, RenderPosition} from './utils/render';
@@ -12,11 +13,7 @@ const TASK_COUNT = 22;
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const siteMenuComponent = new SiteMenuComponent();
-
-siteMenuComponent.getElement().querySelector(`.control__label--new-task`)
-  .addEventListener(`click`, () => {
-    boardController.createTask();
-  });
+const statisticsComponent = new StatisticsComponent();
 
 render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 const tasks = generateTasks(TASK_COUNT);
@@ -28,7 +25,28 @@ filterController.render();
 
 const boardComponent = new BoardComponent();
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
+render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
 
 const boardController = new BoardController(boardComponent, tasksModel);
 
+statisticsComponent.hide();
 boardController.render();
+
+siteMenuComponent.setOnChange((menuItem) => {
+  switch (menuItem) {
+    case MenuItem.NEW_TASK:
+      siteMenuComponent.setActiveItem(MenuItem.TASKS);
+      statisticsComponent.hide();
+      boardController.show();
+      boardController.createTask();
+      break;
+    case MenuItem.STATISTICS:
+      boardController.hide();
+      statisticsComponent.show();
+      break;
+    case MenuItem.TASKS:
+      statisticsComponent.hide();
+      boardController.show();
+      break;
+  }
+});
